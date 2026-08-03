a3b8789f74563646bfa54d5376883614db1342ea
Andrew Jackson <andrew@codechimp.org>
2026-08-03 11:19:14 +0100

    Refactor get_plant_summary to assert device presence and ensure single device for config entry

--- custom_components/plant_monitor_plus/services.py
+++ custom_components/plant_monitor_plus/services.py
@@ -99,11 +99,15 @@ def get_plant_summary(
         devices: list[dr.DeviceEntry] = dr.async_entries_for_config_entry(
             device_registry, config_entry.entry_id
         )
-        for device in devices:
-            # TODO: Change to device.config_entry_id in HA 2026.8
-            if config_entry.entry_id in device.config_entries:
-                device_id = device.id
-                break
+
+        assert devices is not None, "Devices should not be None"
+        assert len(devices) == 1, "There should be only one device for the config entry"
+
+        device = devices[0]
+        # TODO: Change to device.config_entry_id in HA 2026.8
+        if config_entry.entry_id in device.config_entries:
+            device_id = device.id
+        assert device_id is not None, "Device ID should not be None"
 
         evaluation = runtime.evaluate_moisture(hass)
         if not evaluation.available:
